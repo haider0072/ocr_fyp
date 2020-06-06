e3ce9565dcf68d46353f6c1d24f69e2304e847e4
haider0072 <55896536+haider0072@users.noreply.github.com>
2020-06-06 08:35:18 -0700

    sign_up_firebase

--- app/src/main/java/com/example/finalyearproject/signup.java
+++ app/src/main/java/com/example/finalyearproject/signup.java
@@ -1,5 +1,6 @@
 package com.example.finalyearproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,18 +10,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class signup extends AppCompatActivity {
-    private EditText username;
 //    private EditText name;
 
-    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,71 +35,80 @@ public class signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         final EditText name = (EditText) findViewById(R.id.myname);
-        Button signup = (Button) findViewById(R.id.signupButton);
         final EditText password = findViewById(R.id.password);
-        username = findViewById(R.id.username);
-//        name = findViewById(R.id.myname);
+        final EditText username = findViewById(R.id.username);
+
+        final Button b_signup = (Button) findViewById(R.id.signupButton);
 
         //init firebase
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference table_user = database.getReference("User");
 
-        signup.setOnClickListener(new View.OnClickListener() {
+        //OnClickListener On Button
+
+        b_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String validemail = "^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$";
-                String validname = "^[a-zA-Z]\\w{0,}$";
-
-
-
+                String emailRegex = "[a-zA-Z0-9_.]+@[a-zA-Z0-9]+.[a-zA-Z]{2,3}[.] {0,1}[a-zA-Z]+";
                 String email = username.getText().toString();
-                String firstname = name.getText().toString();
+                String pwd = password.getText().toString();
+                final String uname = name.getText().toString();
+                if(email.isEmpty()){
+                    username.setError("Please Enter username");
+                    username.requestFocus();
+                }
+                else if(pwd.isEmpty()){
+                    password.setError("Please Enter password");
+                    password.requestFocus();
+                }
 
-                Matcher matcher= Pattern.compile(validemail).matcher(email);
-                Matcher matcher1= Pattern.compile(validname).matcher(firstname);
+                else if(uname.isEmpty()){
+                    name.setError("Please Enter name");
+                    name.requestFocus();
+                }
+                else if(email.isEmpty() && pwd.isEmpty() && uname.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Fields are empty",Toast.LENGTH_LONG).show();
+                }
 
+                else if(!email.isEmpty() && pwd.isEmpty() && uname.isEmpty() && email.matches(emailRegex)){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email,pwd)
+                            .addOnCompleteListener(signup.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        HashMap<String,String> mp = new HashMap<>();
+                                        mp.put("name",uname);
+                                        FirebaseUser userID=mFirebaseAuth.getCurrentUser();
+                                        databaseReference.child("Users").child(userID.getUid()).setValue(mp);
+                                        startActivity(new Intent(signup.this,login.class));
+                                        Toast.makeText(getBaseContext(),"Successfully Registered",Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(getBaseContext(),"Something went wrong",Toast.LENGTH_LONG).show();
+                                    }
 
-                if (matcher.matches() && isValidPassword(password.getText().toString().trim())){
-                    Toast.makeText(getApplicationContext(),"Successful",Toast.LENGTH_LONG).show();
-                    Intent myIntent = new Intent(v.getContext(),result_activity.class);
-                    startActivityForResult(myIntent,0);
-
+                                }
+                            });
 
                 }
-                else if (!matcher.matches()){
-                    username.setError("example: abc@abc.com");
 
-                }
-                else if (!isValidPassword(password.getText().toString().trim())){
-                    password.setError("At least one numeric character");
-
-                }
-                else if (!matcher1.matches()){
-                    name.setError("This Field Is Empty");
-
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),"Enter Valid Email-Id",Toast.LENGTH_LONG).show();
-                }
             }
 
 
         });
     }
 
-    public boolean isValidPassword(final String password) {
-
-        Pattern pattern;
-        Matcher matcher1;
-
-        final String PASSWORD_PATTERN = "^(?=.*\\d).{4,}$";
-
-        pattern = Pattern.compile(PASSWORD_PATTERN);
-        matcher1 = pattern.matcher(password);
-
-        return matcher1.matches();
-
-    }
+//    public boolean isValidPassword(final String password) {
+//
+//        Pattern pattern;
+//        Matcher matcher1;
+//
+//        final String PASSWORD_PATTERN = "^(?=.*\\d).{4,}$";
+//
+//        pattern = Pattern.compile(PASSWORD_PATTERN);
+//        matcher1 = pattern.matcher(password);
+//
+//        return matcher1.matches();
+//
+//    }
 }
